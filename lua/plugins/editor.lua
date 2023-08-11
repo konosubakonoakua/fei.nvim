93e06f775fb501613a8ec35a3d77bf0907f8b57b
konosubakonoakua <ailike_meow@qq.com>
2023-08-12 03:15:07 +0800

    feat(editor): add telescope file browser

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,7 +35,7 @@ return {
         ["<leader><tab>"] = { name = "+tabs" },
         ["<leader>l"] = { name = "*TBD*" }, -- TODO: remap <leader>l
         ["<leader>L"] = { name = "*TBD*" }, -- TODO: remap <leader>L
-        ["<leader>;"] = { name = "+*TBD*" }, -- TODO: rename <leader>;
+        ["<leader>;"] = { name = "+utils" },
       },
     },
   },
@@ -56,6 +56,17 @@ return {
     },
   },
 
+  -- search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sR", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    },
+  },
+
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
@@ -64,6 +75,7 @@ return {
       -- stylua: ignore start
       { "<leader>fP", function() require("telescope.builtin").find_files( { cwd = require("lazy.core.config").options.root }) end, desc = "Find Plugin File", },
       { "<leader>uc", Util.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
+      { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "Resume" },
       -- stylua: ignore end
     },
     -- change some options
@@ -98,6 +110,86 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
+  },
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    init = function()
+      local fb_actions = require("telescope._extensions.file_browser.actions")
+      require("telescope").setup({
+        extensions = {
+          file_browser = {
+            cwd_to_path = false,
+            grouped = true,
+            files = true,
+            add_dirs = true,
+            depth = 1,
+            auto_depth = false,
+            select_buffer = false,
+            hidden = { file_browser = false, folder_browser = false },
+            hide_parent_dir = false,
+            collapse_dirs = false,
+            prompt_path = true,
+            quiet = false,
+            dir_icon = "",
+            dir_icon_hl = "Default",
+            display_stat = { date = true, size = true, mode = true },
+            hijack_netrw = false,
+            use_fd = true,
+            git_status = false,
+            mappings = {
+              ["i"] = {
+                ["<A-c>"] = fb_actions.create,
+                ["<S-CR>"] = fb_actions.create_from_prompt,
+                ["<A-r>"] = fb_actions.rename,
+                ["<A-m>"] = fb_actions.move,
+                ["<A-y>"] = fb_actions.copy,
+                ["<A-d>"] = fb_actions.remove,
+                ["<C-o>"] = fb_actions.open,
+                ["<C-g>"] = fb_actions.goto_parent_dir,
+                ["<C-e>"] = fb_actions.goto_home_dir,
+                ["<C-w>"] = fb_actions.goto_cwd,
+                ["<C-t>"] = fb_actions.change_cwd,
+                ["<C-f>"] = fb_actions.toggle_browser,
+                ["<C-h>"] = fb_actions.toggle_hidden,
+                ["<C-s>"] = fb_actions.toggle_all,
+                ["<bs>"] = fb_actions.backspace,
+              },
+              ["n"] = {
+                ["c"] = fb_actions.create,
+                ["r"] = fb_actions.rename,
+                ["m"] = fb_actions.move,
+                ["y"] = fb_actions.copy,
+                ["d"] = fb_actions.remove,
+                ["o"] = fb_actions.open,
+                ["g"] = fb_actions.goto_parent_dir,
+                ["e"] = fb_actions.goto_home_dir,
+                ["w"] = fb_actions.goto_cwd,
+                ["t"] = fb_actions.change_cwd,
+                ["f"] = fb_actions.toggle_browser,
+                ["h"] = fb_actions.toggle_hidden,
+                ["s"] = fb_actions.toggle_all,
+              },
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("file_browser")
+      vim.api.nvim_set_keymap(
+        "n",
+        "<space>;f",
+        ":Telescope file_browser<CR>",
+        { desc = "Telescope file_browser", noremap = true }
+      )
+      -- open file_browser with the path of the current buffer
+      vim.api.nvim_set_keymap(
+        "n",
+        "<space>;c",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { desc = "Telescope cwd file_browser", noremap = true }
+      )
+    end,
   },
 
   -- TODO: rework needed
