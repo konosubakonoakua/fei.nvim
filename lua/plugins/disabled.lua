bb9984ac966cd2aeb368f654e0c5c68aed115059
konosubakonoakua <ailike_meow@qq.com>
2023-08-12 03:14:18 +0800

    feat(disable): disable unused plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,18 @@
 -- put disable plugins here
 return {
-  -- -- disable trouble
-  -- { "folke/trouble.nvim", enabled = false }
+  -- disable unused colorschemes
+  { "projekt0n/github-nvim-theme", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- disable neo-tree & edgy
+  -- edgy disabled in lazy.lua (no import)
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "folke/edgy.nvim", enabled = false },
+
+  -- disable zen mode
+  { "folke/zen-mode.nvim", enabled = false },
+
+  -- disable windows resizing
+  { "anuvyklack/windows.nvim", enabled = false },
 }
