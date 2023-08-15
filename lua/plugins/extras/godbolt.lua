11a82bab43e2c86cf66598b6397b143c2d7e8561
konosubakonoakua <ailike_meow@qq.com>
2023-08-16 06:22:50 +0800

    feat(editor): add godbolt support

---
+++ lua/plugins/extras/godbolt.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "konosubakonoakua/godbolt.nvim",
+    lazy = true,
+    init = function()
+      vim.b.godbolt_exec = 0
+
+      require("util").keymap("n", "<leader>;e", function()
+        if vim.b.godbolt_exec then
+          vim.b.godbolt_exec = 0
+          vim.notify("godbolt execute output disabled", vim.log.levels.WARN)
+        else
+          vim.b.godbolt_exec = 1
+          vim.notify("godbolt execute output enabled", vim.log.levels.WARN)
+        end
+      end)
+
+      require("godbolt").setup({
+        languages = {
+          cpp = { compiler = "g122", options = {} },
+          c = { compiler = "cg122", options = {} },
+          rust = { compiler = "r1650", options = {} },
+          -- any_additional_filetype = { compiler = ..., options = ... },
+        },
+        quickfix = {
+          enable = true, -- whether to populate the quickfix list in case of errors
+          auto_open = true, -- whether to open the quickfix list in case of errors
+        },
+        url = "https://godbolt.org", -- can be changed to a different godbolt instance
+      })
+    end,
+  },
+}
