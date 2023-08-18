bb78b0e5301b06cbecb7e3bf1300475ad71e3438
konosubakonoakua <ailike_meow@qq.com>
2023-08-19 04:50:22 +0800

    feat(util): add custom icons

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,18 @@ local spec = {
 
 require("lazy").setup({
   spec = spec,
+  opts = {
+    colorscheme = "oxocarbon",
+    -- load the default settings
+    defaults = {
+      autocmds = true, -- lazyvim.config.autocmds
+      keymaps = true, -- lazyvim.config.keymaps
+      -- lazyvim.config.options can't be configured here since that's loaded before lazyvim setup
+      -- if you want to disable loading options, add `package.loaded["lazyvim.config.options"] = true` to the top of your init.lua
+    },
+    -- icons used by other plugins
+    icons = require("util.icons"),
+  },
   defaults = {
     lazy = false,
     version = false, -- always use the latest git commit
