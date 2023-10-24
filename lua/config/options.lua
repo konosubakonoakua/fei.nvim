1fe2ac0be7b9be7b470c47ee42c04390143570aa
konosubakonoakua <ailike_meow@qq.com>
2023-10-24 12:34:51 +0800

    feat(neovide): add neovide config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,22 @@
 local plat = require("platform")
 
 if vim.g.neovide then
+  -- font
   vim.opt.guifont = { "Lilex Nerd Font Mono", "h8" }
-  -- vim.g.neovide_scale_factor = 1.2
+  vim.g.neovide_scale_factor = 1.0
+
+  -- windows
+  vim.g.neovide_remember_window_size = true
+
+  -- cursor
+  -- vim.g.neovide_cursor_animation_length = 0.13
+  -- vim.g.neovide_cursor_trail_size = 0.8
+  -- vim.g.neovide_cursor_antialiasing = true
+  -- vim.g.neovide_cursor_animate_command_line = true
+  -- vim.g.neovide_cursor_unfocused_outline_width = 0.125
+
+  -- vfx
+  -- vim.g.neovide_cursor_vfx_mode = "railgun"
 end
 
 vim.opt.exrc = true
