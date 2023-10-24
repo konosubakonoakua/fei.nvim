4f457adca06a7d8e2659e70a88dc3877d40ba791
konosubakonoakua <ailike_meow@qq.com>
2023-10-24 23:05:55 +0800

    fix(keymap): fix format toggle keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -159,7 +159,7 @@ keymap("n", "<leader>us", function() _util.toggle("spell") end,      { desc = "T
 keymap("n", "<leader>uw", function() _util.toggle("wrap") end,       { desc = "Toggle Word Wrap" })
 keymap("n", "<leader>ul", function() _util.toggle.number() end,      { desc = "Toggle Line Numbers" })
 keymap("n", "<leader>ud", function() _util.toggle.diagnostics() end, { desc = "Toggle Diagnostics" })
-keymap("n", "<leader>uf", function() require("lazyvim.plugins.lsp.format").toggle() end,         { desc = "Toggle format on Save" })
+keymap("n", "<leader>uf", function() require("lazyvim.util).format").toggle() end, { desc = "Toggle format on Save" })
 
 -- #endregion toggle options
 
