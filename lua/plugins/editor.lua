3671685d0fd5bc36c892bab18a2fa298937ce36f
konosubakonoakua <ailike_meow@qq.com>
2023-11-02 00:46:05 +0800

    fix(editor): add tostring to handle nil

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -74,7 +74,7 @@ return {
     end,
     init = function()
       if vim.fn.argc(-1) == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
         if stat and stat.type == "directory" then
           require("neo-tree")
         end
