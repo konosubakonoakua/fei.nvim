ea6e14111151815e2e3fdef290065d9efc1ca86a
konosubakonoakua <ailike_meow@qq.com>
2023-11-02 00:51:03 +0800

    feat(extras): add zoxide externsion

---
+++ lua/plugins/extras/telescope/zoxide.lua
@@ -0,0 +1,37 @@
+return {
+  "jvgrootveld/telescope-zoxide",
+  config = function(_, opts)
+    local t = require("telescope")
+    local z_utils = require("telescope._extensions.zoxide.utils")
+
+    -- Configure the extension
+    opts = {
+      extensions = {
+        zoxide = {
+          prompt_title = "🙉 Zoxide",
+          mappings = {
+            default = {
+              after_action = function(selection)
+                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+              end,
+            },
+            ["<C-s>"] = {
+              before_action = function(selection)
+                print("before C-s")
+              end,
+              action = function(selection)
+                -- vim.cmd.edit(selection.path)
+                require("neo-tree.command").execute({ toggle = false, dir = vim.loop.cwd() })
+              end,
+            },
+            -- ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+          },
+        },
+      },
+    }
+
+    t.setup(opts)
+    t.load_extension("zoxide")
+    vim.keymap.set("n", "<leader><space>z", t.extensions.zoxide.list)
+  end,
+}
