e3d8e9b2011d52622d827a18340d88773cf8ee19
konosubakonoakua <ailike_meow@qq.com>
2023-11-08 01:46:19 +0800

    refactor(extras): move neocomposer from editor to extras

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -201,7 +201,6 @@ return {
       -- stylua: ignore end
       local telescope = require("telescope")
       telescope.setup(opts)
-      require("telescope").load_extension("macros")
     end,
   },
 
@@ -230,41 +229,6 @@ return {
         },
       })
       require("telescope").load_extension("live_grep_args")
-    end,
-  },
-  -- INFO: yanky on windows put sqlite to disabled, so force enable here
-  { "kkharji/sqlite.lua", enabled = true },
-  {
-    "ecthelionvi/NeoComposer.nvim",
-    opts = {
-      notify = false,
-      delay_timer = 0,
-      queue_most_recent = true,
-      window = {
-        border = "rounded",
-        winhl = {
-          Normal = "ComposerNormal",
-        },
-      },
-      colors = {
-        bg = "#16161e",
-        fg = "#ff9e64",
-        red = "#ec5f67",
-        blue = "#5fb3b3",
-        green = "#99c794",
-      },
-      keymaps = {
-        play_macro = "Q",
-        yank_macro = "yq",
-        stop_macro = "cq",
-        toggle_record = "q",
-        cycle_next = "<c-n>",
-        cycle_prev = "<c-p>",
-        toggle_macro_menu = "<m-q>",
-      },
-    },
-    init = function()
-      require("telescope").load_extension("macros")
     end,
   },
 
---
+++ lua/plugins/extras/editor/neocomposer.lua
@@ -0,0 +1,37 @@
+return {
+  -- INFO: yanky on windows put sqlite to disabled, so force enable here
+  { "kkharji/sqlite.lua", enabled = true },
+  {
+    "ecthelionvi/NeoComposer.nvim",
+    opts = {
+      notify = false,
+      delay_timer = 0,
+      queue_most_recent = true,
+      window = {
+        border = "rounded",
+        winhl = {
+          Normal = "ComposerNormal",
+        },
+      },
+      colors = {
+        bg = "#16161e",
+        fg = "#ff9e64",
+        red = "#ec5f67",
+        blue = "#5fb3b3",
+        green = "#99c794",
+      },
+      keymaps = {
+        play_macro = "Q",
+        yank_macro = "yq",
+        stop_macro = "cq",
+        toggle_record = "q",
+        cycle_next = "<c-n>",
+        cycle_prev = "<c-p>",
+        toggle_macro_menu = "<m-q>",
+      },
+    },
+    init = function()
+      require("telescope").load_extension("macros")
+    end,
+  },
+}
