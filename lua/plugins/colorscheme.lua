e4d095959abf68dc51d166699c6e34b34729c3ed
konosubakonoakua <ailike_meow@qq.com>
2023-11-09 18:37:25 +0800

    feat(theme): add adwaita colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,20 @@ return {
   { "nyoom-engineering/oxocarbon.nvim" },
   { "projekt0n/github-nvim-theme" },
   {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.adwaita_darker = true
+      vim.g.adwaita_disable_cursorline = true
+      vim.g.adwaita_transparent = true
+      -- vim.cmd("colorscheme adwaita")
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "adwaita",
       colorscheme = "oxocarbon",
       -- colorscheme = "github_dark",
     },
