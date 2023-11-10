2949da24a2802d2199869cff35981d97660f17d0
konosubakonoakua <ailike_meow@qq.com>
2023-11-10 15:06:03 +0800

    feat(cmp): only use tab for luasnip

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -64,24 +64,25 @@ return {
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            -- cmp.confirm({ select = true })
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
+          -- -- tweak tab behaviour, only use it for snip
+          -- if cmp.visible() then
+          --   -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+          --   -- cmp.confirm({ select = true })
+          --   cmp.select_next_item()
+          --   -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          --   -- this way you will only jump inside the snippet region
+          if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
+          -- elseif has_words_before() then
+          --   cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+          -- if cmp.visible() then
+          --   cmp.select_prev_item()
+          if luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
@@ -94,6 +95,7 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
+      opts.completion.completeopt = "menu,menuone,noinsert"
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<UP>"] = cmp.mapping.scroll_docs(-4),
         ["<DOWN>"] = cmp.mapping.scroll_docs(4),
