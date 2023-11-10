90128c1825841230a94d8f70d4d5b7b4cf460f0e
konosubakonoakua <ailike_meow@qq.com>
2023-11-10 18:40:42 +0800

    remove <leader>fe/E

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,19 +41,31 @@ return {
       {
         "<leader>fe",
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+          vim.notify(require("lazyvim.util").root())
         end,
         desc = "Explorer NeoTree (root dir)",
       },
       {
         "<leader>fE",
         function()
+          vim.notify(vim.loop.cwd())
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>E",
+        function()
           require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
         end,
         desc = "Explorer NeoTree (cwd)",
       },
-      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
       {
         "<leader>ge",
         function()
