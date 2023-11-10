ab4465efefb6685c4bcac9850eed8f070467000a
konosubakonoakua <ailike_meow@qq.com>
2023-11-10 10:16:22 +0800

    fix(project): disable project manual mode
    https://github.com/LazyVim/LazyVim/discussions/1978

--- lua/plugins/extras/editor/project.lua
+++ lua/plugins/extras/editor/project.lua
@@ -3,12 +3,12 @@ return {
     "ahmedkhalf/project.nvim",
     version = false,
     opts = {
-      manual_mode = true,
+      manual_mode = false,
     },
     event = "VeryLazy",
     config = function(_, opts)
-      opts.detection_methods = { "pattern" }
-      opts.patterns = { ".git" }
+      -- opts.detection_methods = { "pattern" }
+      -- opts.patterns = { ".git" }
       require("project_nvim").setup(opts)
       require("lazyvim.util").on_load("telescope.nvim", function()
         require("telescope").load_extension("projects")
