43c628f77c227363c967bc5aa2eee9bbcd27befc
konosubakonoakua <ailike_meow@qq.com>
2023-11-10 18:45:53 +0800

    fix(editor): add more project patterns

--- lua/plugins/extras/editor/project.lua
+++ lua/plugins/extras/editor/project.lua
@@ -7,8 +7,26 @@ return {
     },
     event = "VeryLazy",
     config = function(_, opts)
-      -- opts.detection_methods = { "pattern" }
-      -- opts.patterns = { ".git" }
+      opts.detection_methods = { "lsp", "pattern" }
+      opts.patterns = {
+        ".git",
+        ".hg",
+        ".svn",
+        ".bzr",
+        "_darcs",
+        "Makefile",
+        "package.json",
+        "CMakeLists.txt",
+        ".toml",
+        ".yaml",
+        "build.zig",
+        "build.bazel",
+        "configure.ac",
+        "autogen.sh",
+        "setup.py",
+        "README",
+        "README.md",
+      }
       require("project_nvim").setup(opts)
       require("lazyvim.util").on_load("telescope.nvim", function()
         require("telescope").load_extension("projects")
