09834122f5d9d5e64240d86d73f9beac75a52bca
konosubakonoakua <ailike_meow@qq.com>
2023-11-24 11:41:46 +0800

    fix(keymap): using `<A-s>` to jump to line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,6 +23,16 @@ local is_disabled_plugin = require("util").is_disabled_plugin
 
 -- #endregion local functions
 
+-- #region flash jump line
+vim.keymap.set({ "i", "x", "n", "s" }, "<A-s>", function()
+  require("flash").jump({
+    search = { mode = "search", max_length = 0 },
+    label = { after = { 0, 0 } },
+    pattern = "^",
+  })
+end, { desc = "line jump" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+-- #endregion flash jump line
 
 -- stay when using * to search
 keymap("n", "*", "*N", _opts)
