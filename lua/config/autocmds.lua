c06c88a2629c2371dee1865a7afbbe6bb96f341b
konosubakonoakua <ailike_meow@qq.com>
2023-11-29 16:17:42 +0800

    feat(autocmds): disable/enable `mini.pairs` when enter/leave macro recording

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,6 +4,9 @@
 -- Add any additional autocmds here
 
 -- local Util = require("lazyvim.util")
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
 
 -- Disable diagnostics in certain files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNew" }, {
@@ -32,3 +35,21 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.wo.conceallevel = 0
   end,
 })
+
+-- Auto disable or enable mini.pairs when using macro
+vim.api.nvim_create_autocmd({ "RecordingEnter" }, {
+  group = augroup("disable_minipairs_when_using_macro"),
+  callback = function(event)
+    vim.g.minipairs_disable = true
+    -- vim.notify("minipairs disabled")
+  end,
+})
+
+-- https://github.com/neovim/neovim/pull/24565
+-- vim.api.nvim_create_autocmd({ "RecordingLeave" }, {
+--   group = augroup("enable_minipairs_when_not_using_macro"),
+--   callback = function(event)
+--     vim.g.minipairs_disable = false
+--     -- vim.notify("minipairs enabled")
+--   end,
+-- })
