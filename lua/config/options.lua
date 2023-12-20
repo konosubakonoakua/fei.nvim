a890d2d1a811620ed773ebc375940d18d3643563
konosubakonoakua <ailike_meow@qq.com>
2023-12-20 14:05:57 +0800

    fix(editor): disable auto format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,3 +45,6 @@ vim.g.dashboard_colorful_banner_chance = 0.001 -- %0.1 chance
 
 -- FIXED: inner word of (???)
 vim.opt.iskeyword:append({ "?" })
+
+-- disable autoformat by default
+vim.g.autoformat = false
