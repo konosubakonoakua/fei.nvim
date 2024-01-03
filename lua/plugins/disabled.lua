0bc5f279a59f68eafa0d6cfa263b7fe8f4afd229
konosubakonoakua <ailike_meow@qq.com>
2024-01-03 20:25:31 +0800

    fix(plugins): add plugin spec doc

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
+-- NOTE: plugin spec
+-- https://github.com/folke/lazy.nvim?tab=readme-ov-file#-plugin-spec
+
 -- PERF: toggle diagnosis at startup
 local Util = require("lazyvim.util")
 vim.diagnostic.disable()
