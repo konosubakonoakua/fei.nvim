cd3b46c9d130abe443ad4ced2025aa29248bd4a9
konosubakonoakua <ailike_meow@qq.com>
2024-01-12 15:59:19 +0800

    fix(disabled): not showing diagnostic disable msg by default

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,6 +4,6 @@
 -- PERF: toggle diagnosis at startup
 local Util = require("lazyvim.util")
 vim.diagnostic.disable()
-Util.warn("Disabled diagnostics", { title = "Diagnostics" })
+-- Util.warn("Disabled diagnostics", { title = "Diagnostics" })
 
 return {}
