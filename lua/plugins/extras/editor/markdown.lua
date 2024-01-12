ec94bb87fdd34c12afca2cd54ee485277c1f15b8
konosubakonoakua <ailike_meow@qq.com>
2024-01-12 15:44:15 +0800

    fix(extras): markdown preview fullscreen

--- lua/plugins/extras/editor/markdown.lua
+++ lua/plugins/extras/editor/markdown.lua
@@ -1,23 +1,23 @@
+-- markdown preview
 return {
-  -- markdown preview
-  {
-    "ellisonleao/glow.nvim",
-    version = false,
-    config = true,
-    cmd = "Glow",
-    keys = {},
-    init = function()
-      require("glow").setup({
-        -- glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
-        -- install_path = "~/.local/bin", -- default path for installing glow binary
-        border = "shadow", -- floating window border config
-        style = "dark", -- filled automatically with your current editor background, you can override using glow json style
-        pager = false,
-        width = 80,
-        height = 100,
-        width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
-        height_ratio = 0.7,
-      })
-    end,
+{
+  "ellisonleao/glow.nvim",
+  version = false,
+  config = true,
+  cmd = "Glow",
+  keys = {},
+  init = function()
+    require("glow").setup({
+      -- glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
+      install_path = "~/.local/bin", -- default path for installing glow binary
+      border = "shadow", -- floating window border config
+      -- pager = "bat",
+      style = "dark", -- filled automatically with your current editor background, you can override using glow json style
+      width = vim.api.nvim_win_get_width(0),
+      height = vim.api.nvim_win_get_height(0),
+      height_ratio = 1,
+      width_ratio = 1, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
+    })
+  end,
   },
 }
