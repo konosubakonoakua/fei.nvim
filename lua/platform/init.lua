4ecb4abdb5f2d3284bcc6f2c4f0926fd51780d05
konosubakonoakua <ailike_meow@qq.com>
2024-01-15 12:26:25 +0800

    fix(platform): make `pwsh` as default shell on windows

--- lua/platform/init.lua
+++ lua/platform/init.lua
@@ -20,4 +20,15 @@ function M.isPlatWindows()
   return jit.os == "Windows"
 end
 
+-- INFO: always use powershell on windows
+if jit.os == "Windows" then
+vim.cmd[[
+  let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+  let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+  let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+  set shellquote= shellxquote=
+]]
+end
+
 return M
