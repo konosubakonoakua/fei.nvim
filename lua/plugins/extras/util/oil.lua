ee20c05c8a6da18c930db25a11812187be973e30
konosubakonoakua <ailike_meow@qq.com>
2024-01-18 18:09:42 +0800

    feat(extras): add `oil`, a buffer-like explorer

---
+++ lua/plugins/extras/util/oil.lua
@@ -0,0 +1,12 @@
+-- Neovim file explorer: edit your filesystem like a buffer
+-- https://github.com/stevearc/oil.nvim
+
+-- vim.keymap.set("n", "<leader>;o", ":Oil<cr>", { desc = "Oil explorer", noremap = true })
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  }
+}
