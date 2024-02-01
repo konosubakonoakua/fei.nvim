2545c0819c55d06891ad2c05fd4f22dc63047417
konosubakonoakua <ailike_meow@qq.com>
2024-02-01 13:01:08 +0800

    fix(terminal): fix #2429, shell cannot quit normally

--- lua/platform/init.lua
+++ lua/platform/init.lua
@@ -1,7 +1,5 @@
 local depspath = vim.fn.stdpath("data") .. "/../nvim-deps"
 if not vim.loop.fs_stat(depspath) then
-  -- FIXME: notify not showing up
-  vim.notify(depspath .. " not found, creating....", 3)
   vim.fn.mkdir(depspath, "p")
 end
 
@@ -22,13 +20,23 @@ end
 
 -- INFO: always use powershell on windows
 if jit.os == "Windows" then
-vim.cmd[[
-  let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-  let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
-  let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-  set shellquote= shellxquote=
-]]
+  vim.cmd[[
+    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+    set shellquote= shellxquote=
+  ]]
+else
+  --  FIXME: #2429 shell not exit normally, related to SHELL path
+  -- extract bash form /bin/bash
+  vim.o.shell = os.getenv("SHELL"):match("([^"..package.config:sub(1,1).."]+)$") or "bash"
+  -- vim.cmd[[
+  --   " let &shell = "/usr/bin/fish"    " not working
+  --   " let &shell = "/bin/bash"        " not working
+  --   " let &shell = "zsh"              " working
+  --   let &shell = "bash"             " working
+  -- ]]
 end
 
 return M
