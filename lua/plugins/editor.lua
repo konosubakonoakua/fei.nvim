7418e5d5dc27ac64c792b6beea844195b7a7fb0e
konosubakonoakua <rafael.ren@foxmail.com>
2024-02-02 14:16:34 +0800

    refactor(telescope): move `live-grep-args` to extras
    this plugin consumes about 11ms at startup

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -596,34 +596,7 @@ return {
     end,
   },
 
-  {
-    "nvim-telescope/telescope-live-grep-args.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    init = function()
-      local lga_actions = require("telescope-live-grep-args.actions")
-      require("telescope").setup({
-        extensions = {
-          live_grep_args = {
-            auto_quoting = true, -- enable/disable auto-quoting
-            -- define mappings, e.g.
-            --          -- FIXME: lvie grep-args keymap not working
-            mappings = { -- extend mappings
-              i = {
-                ["<A-k>"] = lga_actions.quote_prompt(),
-                ["<A-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-              },
-            },
-            -- ... also accepts theme settings, for example:
-            -- theme = "dropdown", -- use dropdown theme
-            -- theme = { }, -- use own theme spec
-            -- layout_config = { mirror=true }, -- mirror preview pane
-          },
-        },
-      })
-      require("telescope").load_extension("live_grep_args")
-    end,
-  },
-
+  -- todo-comments
   -- TODO: ###
   -- FIX: ###
   -- HACK: ###
@@ -631,8 +604,6 @@ return {
   -- PERF: ###
   -- NOTE: ###
   -- TEST: ###
-
-  -- todo-comments
   {
     "folke/todo-comments.nvim",
     version = false,
