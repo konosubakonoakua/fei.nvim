6f672cc08eff7314e269fdf9a1b42ea7a472fef6
konosubakonoakua <ailike_meow@qq.com>
2024-02-05 23:02:07 +0800

    fix(extras): correct `indent` config

--- lua/plugins/extras/editor/indent.lua
+++ lua/plugins/extras/editor/indent.lua
@@ -2,23 +2,23 @@ local M = {
   'nmac427/guess-indent.nvim',
 }
 
-M.opts = function (_, opts)
-  opts.auto_cmd = true  -- Set to false to disable automatic execution
-  opts.override_editorconfig = false -- Set to true to override settings set by .editorconfig
-  opts.filetype_exclude = {  -- A list of filetypes for which the auto command gets disabled
+M.opts = {
+  auto_cmd = true,  -- Set to false to disable automatic execution
+  override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+  filetype_exclude = {  -- A list of filetypes for which the auto command gets disabled
     "netrw",
     "tutor",
-  }
-  opts.buftype_exclude = {  -- A list of buffer types for which the auto command gets disabled
+  },
+  buftype_exclude = {  -- A list of buffer types for which the auto command gets disabled
     "help",
     "nofile",
     "terminal",
     "prompt",
-  }
-  return opts
-end
-
-M.keys = {
+  },
 }
+
+M.lazy = true
+M.keys = {}
+M.event = { "BufEnter" }
 
 return { M }
