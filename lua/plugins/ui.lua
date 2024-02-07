4e98f90a80725d16c2e57479099744ffb5003a50
konosubakonoakua <rafael.ren@foxmail.com>
2024-02-07 19:53:09 +0800

    feat(indent): disable `line_num` for `hlchunk` and `mini.indentscope`

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -292,13 +292,13 @@ return {
         },
 
         line_num = {
-          enable = true,
-          use_treesitter = false,
+          enable = false,
+          use_treesitter = true,
           style = "#806d9c",
         },
 
         blank = {
-          enable = true,
+          enable = false,
           chars = { " ", },
           style = { vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"), },
         },
@@ -393,5 +393,38 @@ return {
       }
       return opts
     end
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    enabled = false,
+    optional = true,
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
 }
