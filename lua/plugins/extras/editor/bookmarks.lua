6d3688aa4205edd6dc624a47ac06641f22019ba2
konosubakonoakua <rafael.ren@foxmail.com>
2024-02-23 13:54:06 +0800

    fix(extras): modify bookmarks.nvim config
    - use own repo until the upstream merged PR
    - change keymaps
    - enable by default in lazyextras

--- lua/plugins/extras/editor/bookmarks.lua
+++ lua/plugins/extras/editor/bookmarks.lua
@@ -1,7 +1,8 @@
 return {
   {
     -- https://github.com/crusj/bookmarks.nvim
-    "crusj/bookmarks.nvim",
+    -- "crusj/bookmarks.nvim",
+    "konosubakonoakua/bookmarks.nvim",
     -- keys = {
     --   { "<tab><tab>", mode = { "n" } },
     -- },
@@ -11,17 +12,18 @@ return {
       require("bookmarks").setup({
         storage_dir = "", -- Default path: vim.fn.stdpath("data").."/bookmarks,  if not the default directory, should be absolute path",
         mappings_enabled = true, -- If the value is false, only valid for global keymaps: toggle、add、delete_on_virt、show_desc
+        -- TODO: reassign keymaps for bookmarks
         keymap = {
-          toggle = "<tab><tab>", -- Toggle bookmarks(global keymap)
+          toggle = "\\\\", -- Toggle bookmarks(global keymap)
           add = "\\z", -- Add bookmarks(global keymap)
           jump = "<CR>", -- Jump from bookmarks(buf keymap)
           delete = "dd", -- Delete bookmarks(buf keymap)
-          order = "<space><space>", -- Order bookmarks by frequency or updated_time(buf keymap)
+          order = "<space>", -- Order bookmarks by frequency or updated_time(buf keymap)
           delete_on_virt = "\\dd", -- Delete bookmark at virt text line(global keymap)
           show_desc = "\\sd", -- show bookmark desc(global keymap)
-          focus_tags = "<c-j>", -- focus tags window
-          focus_bookmarks = "<c-k>", -- focus bookmarks window
-          toogle_focus = "<S-Tab>", -- toggle window focus (tags-window <-> bookmarks-window)
+          -- focus_tags = "", -- focus tags window
+          -- focus_bookmarks = "", -- focus bookmarks window
+          toogle_focus = "<tab>", -- toggle window focus (tags-window <-> bookmarks-window)
         },
         width = 0.8, -- Bookmarks window width:  (0, 1]
         height = 0.7, -- Bookmarks window height: (0, 1]
@@ -31,7 +33,15 @@ return {
 
         virt_text = "", -- Show virt text at the end of bookmarked lines, if it is empty, use the description of bookmarks instead.
         sign_icon = "󰃃", -- if it is not empty, show icon in signColumn.
-        virt_pattern = { "*.go", "*.lua", "*.sh", "*.php", "*.rs" }, -- Show virt text only on matched pattern
+        virt_pattern = { -- Show virt text only on matched pattern
+          "*.c",
+          "*.cpp",
+          "*.go",
+          "*.lua",
+          "*.sh",
+          "*.php",
+          "*.rs"
+        },
         virt_ignore_pattern = {}, -- Ignore showing virt text on matched pattern, this works after virt_pattern
         border_style = "rounded", -- border style: "single", "double", "rounded"
         -- hl = {
