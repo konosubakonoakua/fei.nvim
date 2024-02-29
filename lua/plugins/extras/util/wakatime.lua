bf9bc7880b16769172d56499e044af85a2d0bd5d
konosubakonoakua <rafael.ren@foxmail.com>
2024-02-29 15:33:46 +0800

    feat(extras): add note for waaktime

--- lua/plugins/extras/util/wakatime.lua
+++ lua/plugins/extras/util/wakatime.lua
@@ -3,6 +3,7 @@
 return {
   'wakatime/vim-wakatime',
   -- NOTE: must disable wakatime during github actions
+  -- NOTE better to keep vpn running in order to update cli
   lazy = require("platform").isPlatGithubAction(),
 }
 
