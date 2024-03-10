6441d7dce8e14e0c062d19d179930d8f6777bbee
konosubakonoakua <ailike_meow@qq.com>
2024-03-10 21:49:02 +0800

    feat(extras): add cusorline highlights

---
+++ lua/plugins/extras/editor/cursorline.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'rasulomaroff/reactive.nvim',
+    config = function()
+      require('reactive').setup {
+        builtin = {
+          cursorline = true,
+          cursor = true,
+          modemsg = true
+        }
+      }
+    end
+  }
+}
+
