c8fa7f99cc19db956bf6af644c7ce35ee5729134
konosubakonoakua <rafael.ren@foxmail.com>
2024-03-11 16:37:05 +0800

    feat(extras): cursorline use rgb_tweak

--- lua/plugins/extras/editor/cursorline.lua
+++ lua/plugins/extras/editor/cursorline.lua
@@ -1,15 +1,57 @@
 return {
   {
-    'rasulomaroff/reactive.nvim',
+    "rasulomaroff/reactive.nvim",
+    enabled = true,
+    keys = {
+      {
+        "<leader>uM",
+        function()
+          vim.cmd("ReactiveToggle")
+          vim.notify("ReactiveToggle", vim.log.levels.WARN)
+        end,
+        desc = "Toggle Reactive Cursorlines",
+      },
+    },
+    event = "VeryLazy",
     config = function()
-      require('reactive').setup {
-        builtin = {
-          cursorline = true,
-          cursor = true,
-          modemsg = true
-        }
-      }
-    end
-  }
+      rgb_tweak = require("util.colors").rgb_tweak
+      rgb_tweak_ratio = 0.25
+      require("reactive").add_preset({
+        name = "my-preset",
+        skip = function()
+          return vim.api.nvim_buf_get_option(0, "buftype") == ""
+        end,
+        static = {
+          winhl = {
+            inactive = {
+              CursorLine = { bg = "#202020" },
+              CursorLineNr = { fg = "#b0b0b0", bg = "#202020" },
+            },
+          },
+        },
+        modes = {
+          i = {
+            winhl = {
+              CursorLine = { bg = rgb_tweak("#42be65", rgb_tweak_ratio) },
+              CursorLineNr = { fg = "#42be65", bg = rgb_tweak("#42be65", rgb_tweak_ratio) },
+            },
+          },
+          -- visual
+          [{ "v", "V", "\x16" }] = {
+            winhl = {
+              CursorLineNr = { fg = "#be95ff" },
+              Visual = { bg = rgb_tweak("#be95ff", rgb_tweak_ratio) },
+            },
+          },
+          -- replace
+          R = {
+            winhl = {
+              CursorLine = { bg = rgb_tweak("#eaca89", rgb_tweak_ratio) },
+              CursorLineNr = { fg = "#eaca89", bg = rgb_tweak("#eaca89", rgb_tweak_ratio) },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
-
