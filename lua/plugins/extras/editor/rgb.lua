88f7d94a525b86c3c54eaa9c13f71849e55971e6
konosubakonoakua <rafael.ren@foxmail.com>
2024-03-11 15:25:06 +0800

    feat(extras): add rgb colorizer/previewer

---
+++ lua/plugins/extras/editor/rgb.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    -- https://github.com/brenoprata10/nvim-highlight-colors
+    'brenoprata10/nvim-highlight-colors',
+    enabled = false, -- not working with folding
+    config = true,
+    -- keys = {
+    --   lua require("nvim-highlight-colors").turnOff()
+    --   lua require("nvim-highlight-colors").turnOn()
+    --   lua require("nvim-highlight-colors").toggle()
+    -- }
+
+  },
+  {
+    -- https://github.com/norcalli/nvim-colorizer.lua.git
+    'norcalli/nvim-colorizer.lua',
+    enabled = true,
+    config = function ()
+      require("colorizer").setup()
+    end
+  },
+}
