a3905ba9737a221df6c61900f935e0de268d4154
konosubakonoakua <ailike_meow@qq.com>
2024-03-18 13:28:29 +0800

    feat(extras): add ime auto-switching zh or en
    switch to en when `vimenter` or `insertleave`
    switch to zh when `vimleave` or `insertenter`

---
+++ lua/plugins/extras/editor/ime.lua
@@ -0,0 +1,29 @@
+if jit.os == "Windows" then
+  vim.cmd([[
+    py import win32api, win32gui
+    py from win32con import WM_INPUTLANGCHANGEREQUEST
+  ]])
+
+  local ime_switch = function (mode)
+    if mode == "en" then
+      return vim.fn.pyeval("win32api.SendMessage(win32gui.GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 0, 0x0409)")
+    elseif mode == "zh" then
+      return vim.fn.pyeval("win32api.SendMessage(win32gui.GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 0, 0x0804)")
+    else
+      return -1
+    end
+  end
+
+  -- vim.keymap.set({'n'}, "<leader><leader><leader>", function() ime_switch("en") end, {})
+  vim.api.nvim_create_autocmd({"VimEnter", "InsertLeave"}, {
+    pattern = "*", callback = function(ev) ime_switch("en") end,
+  })
+
+  vim.api.nvim_create_autocmd({"VimLeave", "InsertEnter"}, {
+    pattern = "*", callback = function(ev) ime_switch("zh") end,
+  })
+
+else -- unix, macos
+end
+
+return {}
