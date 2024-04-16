4885d02209ba314eb948d8b42ea6a57c492b42f9
konosubakonoakua <rafael.ren@foxmail.com>
2024-04-16 11:30:44 +0800

    feat(lsp): enable `auto_brackets` in lazyvim cmp for python

--- lua/plugins/extras/lsp/python.lua
+++ lua/plugins/extras/lsp/python.lua
@@ -127,4 +127,11 @@ return {
       },
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "python")
+    end,
+  },
 }
