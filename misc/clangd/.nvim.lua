c912d6a4e540c9a94b9c454c36a77f59426d8b79
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-06 15:41:40 +0800

    feat(lsp/clangd): update clangd settings

--- misc/clangd/.nvim.lua
+++ misc/clangd/.nvim.lua
@@ -1,12 +1,21 @@
 require("lspconfig").clangd.setup({
 	cmd = {
 		"clangd",
+
+		-- speed
+		"-j=8",
+		"--malloc-trim",
 		"--background-index",
+		"--pch-storage=memory",
 		"--clang-tidy",
+
+		-- style
 		"--header-insertion=iwyu",
 		"--completion-style=detailed",
 		"--function-arg-placeholders",
 		"--fallback-style=llvm",
+
+		-- cross-compile
 		-- "--query-driver=/usr/bin/c++",
 		-- "--query-driver=arm-none-eabi-*",
 		"--query-driver=arm-none-linux-gnueabihf-*",
