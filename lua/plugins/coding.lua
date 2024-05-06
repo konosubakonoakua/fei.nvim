7c3e2efe3c57856de6b15d86a1fe2dfd2f1538a5
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-06 10:46:55 +0800

    feat(cmp/snippet): use `native snippets` for `neovim >= 0.10.0`

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,64 +1,68 @@
 local icon_kinds = require("lazyvim.config").icons.kinds
 
 return {
+  -- region NOTE: use Native snippets for Neovim >= 0.10.0
+  -- TODO: remove this region when native snippets is stable
+  --
   -- extend auto completion
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-    opts = function(_, opts)
-      opts.history = false
-      opts.delete_check_events = "TextChanged"
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          -- -- tweak tab behaviour, only use it for snip
-          -- if cmp.visible() then
-          --   -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          --   -- cmp.confirm({ select = true })
-          --   cmp.select_next_item()
-          --   -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          --   -- this way you will only jump inside the snippet region
-          if luasnip.expand_or_locally_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-            -- luasnip.expand_or_jump()
-            -- elseif has_words_before() then
-            --   cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          -- if cmp.visible() then
-          --   cmp.select_prev_item()
-          if luasnip.jumpable(-1) then
-            -- luasnip.jump(-1)
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  --   opts = function(_, opts)
+  --     opts.history = false
+  --     opts.delete_check_events = "TextChanged"
+  --   end,
+  -- },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-emoji",
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local has_words_before = function()
+  --       unpack = unpack or table.unpack
+  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --     end
+  --
+  --     local luasnip = require("luasnip")
+  --     local cmp = require("cmp")
+  --
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         -- -- tweak tab behaviour, only use it for snip
+  --         -- if cmp.visible() then
+  --         --   -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+  --         --   -- cmp.confirm({ select = true })
+  --         --   cmp.select_next_item()
+  --         --   -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+  --         --   -- this way you will only jump inside the snippet region
+  --         if luasnip.expand_or_locally_jumpable() then
+  --           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+  --           -- luasnip.expand_or_jump()
+  --           -- elseif has_words_before() then
+  --           --   cmp.complete()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --         -- if cmp.visible() then
+  --         --   cmp.select_prev_item()
+  --         if luasnip.jumpable(-1) then
+  --           -- luasnip.jump(-1)
+  --           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --     })
+  --   end,
+  -- },
+  -- endregion
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
