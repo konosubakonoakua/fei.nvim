922ef60068b2d83389178a5e2bf0c5bc9c6af835
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-23 17:00:38 +0800

    feat(extras/git): try `mini-git`

---
+++ lua/plugins/extras/util/git/mini-git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'echasnovski/mini-git',
+    version = false,
+    main = 'mini.git',
+    opts = {
+      -- General CLI execution
+      job = {
+        -- Path to Git executable
+        git_executable = 'git',
+
+        -- Timeout (in ms) for each job before force quit
+        timeout = 30000,
+      },
+
+      -- Options for `:Git` command
+      command = {
+        -- Default split direction
+        split = 'auto',
+      },
+    },
+  },
+}
