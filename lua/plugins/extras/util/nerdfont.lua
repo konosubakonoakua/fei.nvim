98b01a475577819eeba89d97e721a35e3e9371c3
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-23 16:45:44 +0800

    fix(extras/font): add `Terdy` command for `Telescope Nerdy`

--- lua/plugins/extras/util/nerdfont.lua
+++ lua/plugins/extras/util/nerdfont.lua
@@ -19,15 +19,17 @@ return {
   },
   {
     '2kabhishek/nerdy.nvim',
-    lazy = true,
-    cmd = 'Nerdy',
     version = false,
     enabled = true,
+    lazy = true,
+    event = "VeryLazy",
+    cmd = {'Terdy', 'Nerdy'},
     dependencies = {
       'stevearc/dressing.nvim',
       'nvim-telescope/telescope.nvim',
     },
     config = function ()
+      vim.api.nvim_create_user_command('Terdy', 'Telescope nerdy', { desc = "Telescope nerd fonts with nerdy"})
       require('telescope').load_extension('nerdy')
     end
   },
