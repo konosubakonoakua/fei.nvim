992b29b47266c400f08a703310abfbae019a13f0
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-23 11:40:04 +0800

    fix(editor/noice): disable bottom search

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -201,4 +201,18 @@ return {
     },
   },
 
+  -- noice
+  {
+    "folke/noice.nvim",
+    opts = function (_, opts)
+      opts.presets = {
+        bottom_search = false,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      }
+      return opts
+    end
+  },
+
 }
