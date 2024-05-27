c2e08da2a26c16d5f447baf6442a17de6dc05d55
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-27 18:55:52 +0800

    feat(config/lazy): add user cmd `LazyrcGenerate`
    copy `~/.config/nvim/.lazy.lua` to `cwd()`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -65,3 +65,25 @@ require("lazy").setup({
     },
   },
 })
+
+-- region lazyrc
+vim.api.nvim_create_user_command('LazyrcGenerate', function ()
+  local uv = vim.uv or vim.loop
+
+  local lazyrc = vim.fn.stdpath("config") .. "/.lazy.lua"
+  local target = vim.fn.getcwd()
+  if not target then
+    LazyVim.error("Cannot read cwd", { title = "Generate .lazy.lua" })
+  else
+    target = target .. "/.lazy.lua"
+  end
+
+  local res = uv.fs_copyfile(lazyrc, target)
+
+  if res then
+    LazyVim.info("Copied `.lazy.lua` to " .. target, { title = "Generate .lazy.lua" })
+  else
+    LazyVim.error("Cannot copy `.lazy.lua` to " .. target, { title = "Generate .lazy.lua" })
+  end
+end, {desc = "generate .lazy.lua (overwrite)"})
+-- endregion lazyrc
