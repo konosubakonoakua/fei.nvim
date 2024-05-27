5f68f3c2b250300aa1a054c06991ffda88a7cfd3
konosubakonoakua <ailike_meow@qq.com>
2024-05-28 01:30:05 +0800

    refactor(config/usercmds): move user commands to `config/usercmds`

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
+-- load usercmds first
+pcall(require, "config/usercmds")
+
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set:
 --   https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
+++ lua/config/usercmds.lua
@@ -0,0 +1,33 @@
+-- region lazyrc
+vim.api.nvim_create_user_command('LazyrcGenerate', function ()
+  local uv = vim.uv or vim.loop
+
+  local lazyrc = vim.fn.stdpath("config") .. "/.lazy.lua"
+  local target = vim.fn.getcwd()
+  if not target then
+    LazyVim.error("Cannot read cwd", { title = "Generate .lazy.lua" })
+  else
+    target = target .. "/.lazy.lua"
+  end
+
+  local res = uv.fs_copyfile(lazyrc, target)
+
+  if res then
+    LazyVim.info("Copied `.lazy.lua` to " .. target, { title = "Generate .lazy.lua" })
+  else
+    LazyVim.error("Cannot copy `.lazy.lua` to " .. target, { title = "Generate .lazy.lua" })
+  end
+end, {desc = "generate .lazy.lua (overwrite)"})
+-- endregion lazyrc
+
+-- region log
+vim.api.nvim_create_user_command('LogOpen', function ()
+  local uv = vim.uv or vim.loop
+  local log = vim.fs.normalize(vim.fn.stdpath("log") .. "/log")
+  local bufnr = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_buf_set_name(bufnr, log)
+  vim.api.nvim_buf_call(bufnr, vim.cmd.view)
+end, {desc = "Open Neovim log"})
+-- endregion log
+
+return {}
