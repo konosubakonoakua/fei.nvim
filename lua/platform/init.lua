1ca9a57a3805e06c436eb3e14aafed8e6201c270
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-28 10:39:53 +0800

    fix(platform): add `CI` checking when get `SHELL`

--- lua/platform/init.lua
+++ lua/platform/init.lua
@@ -35,21 +35,23 @@ if jit.os == "Windows" then
 else
   --  FIXME: #2429 shell not exit normally, related to SHELL path
   -- extract bash form /bin/bash
-  vim.o.shell = os.getenv("SHELL"):match("([^"..package.config:sub(1,1).."]+)$") or "bash"
-  -- vim.cmd[[
-  --   " let &shell = "/usr/bin/fish"    " not working
-  --   " let &shell = "/bin/bash"        " not working
-  --   " let &shell = "zsh"              " working
-  --   let &shell = "bash"             " working
-  -- ]]
+  -- NOTE: CI bug: /home/runner/.config/nvim/lua/platform/init.lua:38: attempt to index a nil value
+  if not M.isPlatGithubAction() then
+    vim.o.shell = os.getenv("SHELL"):match("([^"..package.config:sub(1,1).."]+)$") or "bash"
+  else
+    vim.cmd [[ let &shell = "bash" ]]
+  end
 end
 
-if vim.fn.executable("tree-sitter") == 0 then
-  LazyVim.error("tree-sitter cli executable not found. you can download it from: https://github.com/tree-sitter/tree-sitter/releases")
-end
 
-if vim.fn.executable("gh") == 0 then
-  LazyVim.error("github cli executable not found. you can download it from: https://github.com/cli/cli/releases")
+if not M.isPlatGithubAction() then
+  if vim.fn.executable("tree-sitter") == 0 then
+    LazyVim.error("tree-sitter cli executable not found. you can download it from: https://github.com/tree-sitter/tree-sitter/releases")
+  end
+
+  if vim.fn.executable("gh") == 0 then
+    LazyVim.error("github cli executable not found. you can download it from: https://github.com/cli/cli/releases")
+  end
 end
 
 return M
