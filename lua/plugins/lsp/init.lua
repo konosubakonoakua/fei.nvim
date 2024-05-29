8484e11948659867fa001e49fbac4f071fc2e7c3
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-29 19:52:04 +0800

    feat(lsp): update `lspconfig` & `mason-lspconfig`
    disable `codelens` & `document_highlight` by default
    enable `inlay_hints` by default

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,18 +3,40 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {},
-      automatic_installation = false,
+      automatic_installation = true,
     },
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      opts.inlay_hints.enabled = true
-      -- filetypes for which you don't want to enable inlay hints
-      opts.inlay_hints.exclude = {
-
-      }
-      return opts
-    end
+    opts = {
+      -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+      -- Be aware that you also will need to properly configure your LSP server to
+      -- provide the inlay hints.
+      inlay_hints = {
+        enabled = true,
+        exclude = {}, -- filetypes for which you don't want to enable inlay hints
+      },
+      -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+      -- Be aware that you also will need to properly configure your LSP server to
+      -- provide the code lenses.
+      codelens = {
+        enabled = false,
+      },
+      -- Enable lsp cursor word highlighting
+      -- will be disabled by `illuminate`
+      document_highlight = {
+        enabled = false, -- slow with neodev when searching a large lua code base
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      -- setup = {
+      --   -- Specify * to use this function as a fallback for any server
+      --   ["*"] = function(server, opts)
+      --     -- do not automatically configure lsp servers installed by mason
+      --     return false -- ture for bypassing or false for using default `lspconfig setup` in LazyVim
+      --   end,
+      -- },
+    },
   },
 }
