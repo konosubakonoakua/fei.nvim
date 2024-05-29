d71b590502b8c54f5cbad5f4214374fecec3c7dd
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-29 11:17:09 +0800

    feat(lsp): add `inlay_hints.exclude`

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,8 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      opts.inlay_hints = {
-        enabled = true,
+      opts.inlay_hints.enabled = true
+      -- filetypes for which you don't want to enable inlay hints
+      opts.inlay_hints.exclude = {
+
       }
       return opts
     end
