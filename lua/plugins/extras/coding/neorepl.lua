4594507ce4856ded0263f63785216839c246f726
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-29 13:35:14 +0800

    fix(extras/neorepl): use `api.nvim_buf_set_keymap` to map `q` for neorepl quiting

--- lua/plugins/extras/coding/neorepl.lua
+++ lua/plugins/extras/coding/neorepl.lua
@@ -1,10 +1,23 @@
 -- https://github.com/ii14/neorepl.nvim
+-- local cmd_neorepl_quit = [[
+-- <cmd>lua << EOF
+-- local repl_bufnr = vim.api.nvim_get_current_buf()
+-- vim.cmd('stopinsert')
+-- require('neorepl.bufs')[repl_bufnr] = nil
+-- vim.api.nvim_buf_delete(repl_bufnr, { force = true })
+-- EOF
+-- <cr>
+-- ]]
+
 return {
   {
     "ii14/neorepl.nvim",
     lazy = true,
     -- event = "WinEnter",
     init = function ()
+      vim.api.nvim_create_user_command('NeoreplQuit', function ()
+      end, {desc = "Neorepl Quit"})
+
       -- NOTE: q to close REPL instance in norm mode
       vim.keymap.set('n', 'g:', function()
         -- get current buffer and window
@@ -19,14 +32,14 @@ return {
           window = win,
         }
 
-        local repl_bufnr = vim.api.nvim_get_current_buf()
         if vim.bo.ft == "neorepl" then
+          -- vim.api.nvim_buf_set_keymap(0, "n", 'q', cmd_neorepl_quit, {desc = "Quit Neorepl"})
           vim.keymap.set("n", 'q', function ()
+            local repl_bufnr = vim.api.nvim_get_current_buf()
             vim.cmd('stopinsert')
             require('neorepl.bufs')[repl_bufnr] = nil
             vim.api.nvim_buf_delete(repl_bufnr, { force = true })
-            return false
-          end)
+          end, { buffer = true, desc = "Quit Neorepl" })
         end
 
         -- resize repl window and make it fixed height
