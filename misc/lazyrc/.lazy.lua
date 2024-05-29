6348f9f16bbce5b4038168da97fbec8029112183
konosubakonoakua <rafael.ren@foxmail.com>
2024-05-29 21:42:38 +0800

    refactor(lazyrc): move `.lazy.lua` to `misc/lazyrc`
    only use `.lazy.lua` to disable time consuming or conflicted plugins

---
+++ misc/lazyrc/.lazy.lua
@@ -0,0 +1,41 @@
+return {
+
+  -- TODO: comment unused extras
+
+  -- debugger
+  -- { import = "lazyvim.plugins.extras.dap.core" },
+  -- { import = "plugins.extras.editor.dap" },
+
+  -- lua
+  -- { import = "lazyvim.plugins.extras.dap.nlua" },
+
+  -- fennel
+  -- { import = "plugins.extras.lang.fennel" },
+
+  -- c/cpp
+  -- { import = "lazyvim.plugins.extras.lang.clangd" },
+  -- { import = "lazyvim.plugins.extras.lang.cmake" },
+  -- { import = "plugins.extras.coding.godbolt" }, -- online compiler
+
+  -- epics
+  -- { import = "plugins.extras.lang.epics" },
+
+  -- python
+  -- { import = "lazyvim.plugins.extras.lang.python" },
+  -- { import = "lazyvim.plugins.extras.lang.python-semshi" },
+  -- { import = "plugins.extras.lang.python" },
+
+  -- rust
+  -- { import = "lazyvim.plugins.extras.lang.rust" },
+  -- { import = "plugins.extras.lang.rust" },
+
+  -- zig
+  -- { import = "plugins.extras.lang.zig" },
+
+  -- latex
+  -- { import = "lazyvim.plugins.extras.lang.tex" },
+
+  -- xmake
+  -- { import = "plugins.extras.build._xmake" },
+
+}
