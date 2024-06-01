08699fc3b3182ed876b541f50f02ecb5e064f82b
konosubakonoakua <ailike_meow@qq.com>
2024-06-02 03:08:09 +0800

    feat(extras/util): add `screenkey.nvim`
    add `<leader>;S`

---
+++ lua/plugins/extras/util/screenkey.lua
@@ -0,0 +1,9 @@
+return {
+    "NStefan002/screenkey.nvim",
+    cmd = "Screenkey",
+    version = "*",
+    config = true,
+    keys = {
+        {"<leader>;S", "<cmd>Screenkey<cr>", mode = "n", desc = "Toggle Screenkey"}
+    },
+}
