d55c420c1e9fc2442b7caab682c9b0d01ffa62a0
konosubakonoakua <ailike_meow@qq.com>
2024-06-02 05:00:05 +0800

    feat(extras/editor): add new macro plugin `nvim-recorder`

--- lua/plugins/extras/editor/macro.lua
+++ lua/plugins/extras/editor/macro.lua
@@ -2,10 +2,11 @@ return {
   -- INFO: yanky on windows put sqlite to disabled, so force enable here
   {
     "kkharji/sqlite.lua",
-    enabled = true
+    enabled = false,
   },
   {
     "ecthelionvi/NeoComposer.nvim",
+    enabled = false,
     version = false,
     opts = {
       notify = false,
@@ -36,7 +37,82 @@ return {
     },
     init = function()
       require("telescope").load_extension("macros")
-      keymap_force("n", "<leader>fm", "<cmd>Telescope macros<cr>",   { desc = "NeoComposer Macros", noremap = true })
+      keymap_force("n", "<leader>fm", "<cmd>Telescope macros<cr>", { desc = "NeoComposer Macros", noremap = true })
+    end,
+  },
+  -- minimal config for lazy-loading with lazy.nvim
+  {
+    "chrisgrieser/nvim-recorder",
+    event = "VeryLazy",
+    dependencies = "rcarriga/nvim-notify",
+    keys = {
+      -- these must match the keys in the mapping config below
+      { "q", desc = "󰑋" },
+      { "Q", desc = "󰑙" },
+    },
+    config = function()
+      vim.cmd[[set shm+=q]] -- hide the `recording ...` message
+      require("recorder").setup({
+        -- Named registers where macros are saved (single lowercase letters only).
+        -- The first register is the default register used as macro-slot after
+        -- startup.
+        slots = { "a", "b" },
+
+        mapping = {
+          startStopRecording = "q",
+          playMacro = "Q",
+          switchSlot = "<C-q>",
+          editMacro = "cq",
+          deleteAllMacros = "dq",
+          yankMacro = "yq",
+          -- ⚠️ this should be a string you don't use in insert mode during a macro
+          addBreakPoint = "##",
+        },
+
+        -- Clears all macros-slots on startup.
+        clear = false,
+
+        -- Log level used for non-critical notifications; mostly relevant for nvim-notify.
+        -- (Note that by default, nvim-notify does not show the levels `trace` & `debug`.)
+        logLevel = vim.log.levels.INFO, -- :help vim.log.levels
+
+        -- If enabled, only essential notifications are sent.
+        -- If you do not use a plugin like nvim-notify, set this to `true`
+        -- to remove otherwise annoying messages.
+        lessNotifications = true,
+
+        -- Use nerdfont icons in the status bar components and keymap descriptions
+        useNerdfontIcons = true,
+
+        -- Performance optimzations for macros with high count. When `playMacro` is
+        -- triggered with a count higher than the threshold, nvim-recorder
+        -- temporarily changes changes some settings for the duration of the macro.
+        performanceOpts = {
+          countThreshold = 100,
+          lazyredraw = true, -- enable lazyredraw (see `:h lazyredraw`)
+          noSystemClipboard = true, -- remove `+`/`*` from clipboard option
+          autocmdEventsIgnore = { -- temporarily ignore these autocmd events
+            "TextChangedI",
+            "TextChanged",
+            "InsertLeave",
+            "InsertEnter",
+            "InsertCharPre",
+          },
+        },
+
+        -- [experimental] partially share keymaps with nvim-dap.
+        -- (See README for further explanations.)
+        dapSharedKeymaps = false,
+      })
+
+      require("lualine").setup({
+        sections = {
+          lualine_z = {
+            require("recorder").recordingStatus,
+            require("recorder").displaySlots,
+          },
+        },
+      })
     end,
   },
 }
