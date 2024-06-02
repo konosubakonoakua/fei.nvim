0ea5da166a4c89feb2dfac5e76dde165261d5d16
konosubakonoakua <ailike_meow@qq.com>
2024-06-02 15:34:45 +0800

    feat(keymap): add `<leader>fl` to find lazy spec

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -212,6 +212,26 @@ return {
       { mode = "n", "<leader>su", "<cmd>Telescope tags<cr>",         desc = "Tags", noremap = true },
       { mode = "n", "<leader>sU", "<cmd>Telescope tagstack<cr>",     desc = "Tagstack", noremap = true },
       { mode = "n", "<leader>sR", function() require("spectre").open() end, desc = "Replace in Files (Spectre)" }, -- NOTE: overwrite LazyVim default mapping for telescope resume
+      { mode = "n", "<leader>fl", function()
+          local files = {} ---@type table<string, string>
+          for _, plugin in pairs(require("lazy.core.config").plugins) do
+            repeat
+              if plugin._.module then
+                local info = vim.loader.find(plugin._.module)[1]
+                if info then
+                  files[info.modpath] = info.modpath
+                end
+              end
+              plugin = plugin._.super
+            until not plugin
+          end
+          require("telescope.builtin").live_grep({
+            default_text = "/",
+            search_dirs = vim.tbl_values(files),
+          })
+        end,
+        desc = "Find Lazy Plugin Spec",
+      },
     },
   },
 
