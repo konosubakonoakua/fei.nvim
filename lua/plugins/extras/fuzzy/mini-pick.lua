679fc37facfd379ca1425441793f95cb5fef6277
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-04 19:15:53 +0800

    feat(extras/fuzzy): try `mini-pick`

---
+++ lua/plugins/extras/fuzzy/mini-pick.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    -- TODO: try mini.pick
+    -- slow when grep in $HOME
+    -- fast when picking files in $HOME
+    'echasnovski/mini.pick',
+    version = false,
+    lazy = false,
+    config = true,
+  },
+}
