1e3a64de66b9ce3ad59081844d0ae3f333779474
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-06 20:42:53 +0800

    feat(extras/git): import Lazyvim octo specs

--- lua/plugins/extras/git/octo.lua
+++ lua/plugins/extras/git/octo.lua
@@ -1,8 +1,75 @@
 return {
+  -- { import = "lazyvim.plugins.extras.util.octo" },
+
   {
-    -- TODO: keymapping for 'octo.nvim'
-    -- "konosubakonoakua/octo.nvim",
+    "echasnovski/mini.diff",
+    keys = function ()
+      return {
+        {
+          "<leader>gm",
+          function()
+            require("mini.diff").toggle_overlay(0)
+          end,
+          desc = "Toggle mini.diff overlay",
+        },
+      }
+    end
+  },
+
+  -- Octo Keys
+  {
+    "pwntester/octo.nvim",
+    init = function()
+      vim.treesitter.language.register("markdown", "octo")
+    end,
+    opts = {
+      picker = "telescope",
+      enable_builtin = true,
+      default_to_projects_v2 = true,
+      default_merge_method = "squash",
+      mappings_disable_default = false, -- disable all default keys
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    },
+    keys = {
+      { "<leader>go", "<cmd>Octo<cr>", desc = "Octo" },
+      -- { "<leader>gi", "<cmd>Octo issue list<CR>", desc = "List Isues (Octo)" },
+      -- { "<leader>gI", "<cmd>Octo issue search<CR>", desc = "Search Isues (Octo)" },
+      -- { "<leader>gp", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+      -- { "<leader>gP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
+      -- { "<leader>gr", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
+      -- { "<leader>gS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+      { "<leader>a", "", desc = "+assignee (Otco)", ft = "octo" },
+      { "<leader>c", "", desc = "+comment/code (Otco)", ft = "octo" },
+      { "<leader>l", "", desc = "+label (Otco)", ft = "octo" },
+      { "<leader>i", "", desc = "+issue (Otco)", ft = "octo" },
+      { "<leader>r", "", desc = "+react (Otco)", ft = "octo" },
+      { "<leader>p", "", desc = "+pr (Otco)", ft = "octo" },
+      { "<leader>v", "", desc = "+review (Otco)", ft = "octo" },
+      { "@", "@<C-x><C-o>", mode = "i", ft = "octo", silent = true },
+      { "#", "#<C-x><C-o>", mode = "i", ft = "octo", silent = true },
+    },
+  },
+
+  -- Octo Picker
+  {
+    "pwntester/octo.nvim",
+    opts = function(_, opts)
+      if LazyVim.has("telescope.nvim") then
+        opts.picker = "telescope"
+      elseif LazyVim.has("fzf-lua") then
+        opts.picker = "fzf-lua"
+      else
+        LazyVim.error("`octo.nvim` requires `telescope.nvim` or `fzf-lua`")
+      end
+    end,
+  },
+
+  -- Octo lazy loading
+  {
     'pwntester/octo.nvim',
+    cmd = "Octo",
     -- event = "VeryLazy", -- don't use event if use module
     module = false, -- NOTE: do not automatically load it
     init = function ()
@@ -12,15 +79,6 @@ return {
         vim.cmd[[Lazy load octo.nvim]]
         LazyVim.info(":Lazy load octo.nvim")
       end, {desc = ":Lazy load octo.nvim"})
-    end,
-    cmd = {"Octo"},
-    config = function()
-      require("octo").setup({
-        mappings_disable_default = false,
-        suppress_missing_scope = {
-          projects_v2 = true,
-        },
-      })
     end,
   },
 }
