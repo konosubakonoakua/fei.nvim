b7ade2f66561e92fd7125409da2788bb966db2eb
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-06 13:15:32 +0800

    feat(extras/git): manually load `octo.nvim` by `:Octo`
    sometimes `octo.nvim` startup time is very long due to network issue

--- lua/plugins/extras/git/octo.lua
+++ lua/plugins/extras/git/octo.lua
@@ -3,7 +3,17 @@ return {
     -- TODO: keymapping for 'octo.nvim'
     -- "konosubakonoakua/octo.nvim",
     'pwntester/octo.nvim',
-    event = "VeryLazy",
+    -- event = "VeryLazy", -- don't use event if use module
+    module = false, -- NOTE: do not automatically load it
+    init = function ()
+      -- call `Octo` first time to load
+      -- then the cmd will be overwritten by setup later
+      vim.api.nvim_create_user_command('Octo', function ()
+        vim.cmd[[Lazy load octo.nvim]]
+        LazyVim.info(":Lazy load octo.nvim")
+      end, {desc = ":Lazy load octo.nvim"})
+    end,
+    cmd = {"Octo"},
     config = function()
       require("octo").setup({
         mappings_disable_default = false,
