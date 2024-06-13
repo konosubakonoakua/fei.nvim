aefd6abc0b26432a012816b4c3a6103c344091ec
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-13 16:56:58 +0800

    feat(util): add `trunc` to util

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -116,4 +116,22 @@ function M.list_insert_unique(tbl, vals)
   end
 end
 
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+function M.trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ""
+    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+      return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+    end
+    return str
+  end
+end
+
+
 return M
