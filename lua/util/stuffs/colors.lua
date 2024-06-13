4ffe1c8f848c0a82f49941adbd4d91c179294e5b
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-13 16:57:15 +0800

    feat(util/colors): add colors helper

--- lua/util/stuffs/colors.lua
+++ lua/util/stuffs/colors.lua
@@ -250,4 +250,44 @@ M.rgb_tweak = function(color, ratio)
   return res
 end
 
+-- region taken from zen-mode
+function M.get_hl(name)
+  local ok, hl = pcall( vim.api.nvim_get_hl, 0,
+    {
+      name = name,
+      link = false, -- false to follow link
+    }
+  )
+  if not ok then
+    return nil
+  end
+  for _, key in pairs({ "fg", "bg" }) do
+    if hl[key] then
+      hl[key] = string.format("#%06x", hl[key])
+    end
+  end
+  return hl
+end
+
+function M.hex2rgb(hex)
+  hex = hex:gsub("#", "")
+  return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
+end
+
+function M.rgb2hex(r, g, b)
+  return string.format("#%02x%02x%02x", r, g, b)
+end
+
+function M.darken(hex, amount)
+  local r, g, b = M.hex2rgb(hex)
+  return M.rgb2hex(r * amount, g * amount, b * amount)
+end
+
+function M.is_dark(hex)
+  local r, g, b = M.hex2rgb(hex)
+  local lum = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+  return lum <= 0.5
+end
+-- endregion taken from zen-mode
+
 return M
