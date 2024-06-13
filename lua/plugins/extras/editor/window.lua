904f8a139342a4bab34eaa0997d3d6922642700c
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-13 21:13:03 +0800

    fix(extras/window): ignore `tressitter-context` for picker

--- lua/plugins/extras/editor/window.lua
+++ lua/plugins/extras/editor/window.lua
@@ -47,7 +47,71 @@ return {
   },
 
   {
+    -- NOTE: useful when debugging
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    keys = {
+      {
+        "<leader>wp",
+        function()
+          -- local _, tsc = pcall(require, "treesitter-context")
+          -- pcall(function () tsc.disable() end)
+
+          local selected_window = require("window-picker").pick_window()
+
+          -- stylua: ignore
+          if selected_window ~= nil
+            and vim.api.nvim_win_is_valid(selected_window)
+            -- and vim.api.nvim_win_get_height(selected_window) > 3
+          then
+            vim.api.nvim_set_current_win(selected_window)
+          end
+
+          -- pcall(function () tsc.enable() end)
+        end,
+        desc = "Window Picker",
+      },
+    },
     config = function()
+      require("window-picker").setup({
+        show_prompt = false,
+        include_current = false,
+        hint = "floating-big-letter",
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = false,
+          bo = { -- ignore
+            filetype = {
+              "neo-tree",
+              "neo-tree-popup",
+              "notify",
+              "edgy",
+              "noice",
+              "NvimSeparator", -- "s1n7ax/nvim-window-picker"
+            },
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+        -- NOTE: find treesitter-context window too, need to exlude by filter, see below
+        -- https://github.com/s1n7ax/nvim-window-picker/issues/67
+        filter_func = function(windows, rules)
+          local function predicate(wid)
+            cfg = vim.api.nvim_win_get_config(wid)
+            if not cfg.focusable then
+              return false
+            end
+            return true
+          end
+          local filtered = vim.tbl_filter(predicate, windows)
+
+          local dfilter = require("window-picker.filters.default-window-filter"):new()
+          dfilter:set_config(rules)
+          return dfilter:filter_windows(filtered)
+        end,
+        border = { style = "rounded", highlight = "Normal" },
+      })
     end,
   },
 
