ddbd715151960e0bf30eac5515214ee24745a8c0
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-14 13:22:44 +0800

    feat(extras/fuzzy): add telescope config to user extras

---
+++ lua/plugins/extras/fuzzy/telescope.lua
@@ -0,0 +1,6 @@
+return {
+  { import = "lazyvim.plugins.extras.editor.fzf", enabled = false },
+  { import = "lazyvim.plugins.extras.editor.telescope", enabled = true},
+  -- telescope picker keymapping
+  { "nvim-telescope/telescope.nvim", },
+}
