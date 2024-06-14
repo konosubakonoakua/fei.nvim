91737c964bb184d7ddb9e367c52cf67d91b95d9c
konosubakonoakua <rafael.ren@foxmail.com>
2024-06-14 13:53:59 +0800

    feat(extras/editor): add cursorline number highlighter based on vim mode

---
+++ lua/plugins/extras/editor/vimmode.lua
@@ -0,0 +1,26 @@
+return({
+  {
+    "mawkler/modicator.nvim",
+    opts = {
+      -- Warn if any required option is missing. May emit false positives if some
+      -- other plugin modifies them, which in that case you can just ignore
+      show_warnings = false,
+      highlights = {
+        -- Default options for bold/italic
+        defaults = {
+          bold = true,
+          italic = false,
+        },
+      },
+      integration = {
+        lualine = {
+          enabled = true,
+          -- Letter of lualine section to use (if `nil`, gets detected automatically)
+          mode_section = nil,
+          -- Whether to use lualine's mode highlight's foreground or background
+          highlight = 'bg',
+        },
+      },
+    }
+  },
+})
