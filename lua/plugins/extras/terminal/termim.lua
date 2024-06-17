aa9baecd70892f776724ebaea3157c5e6f5dd667
konosubakonoakua <ailike_meow@qq.com>
2024-06-17 20:10:55 +0800

    feat(terminal): add `termim` to extras/terminal

---
+++ lua/plugins/extras/terminal/termim.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "2KAbhishek/termim.nvim",
+    version = "*",
+    lazy = true,
+    cmd = { "Fterm", "FTerm", "Sterm", "STerm", "Vterm", "VTerm" },
+    keys = {
+      { "<leader><leader>s", "<cmd>Sterm<cr>", desc = "Horizontal Terminal", silent = true, },
+      { "<leader><leader>v", "<cmd>Vterm<cr>", desc = "Vertical Terminal", silent = true, },
+      -- { "<leader><leader>g", "<cmd>Fterm lazygit<cr>", desc = "Lazygit" },
+      -- { "<leader><leader>t", "<cmd>Fterm<cr>", desc = "Float Terminal" },
+
+    },
+  },
+}
