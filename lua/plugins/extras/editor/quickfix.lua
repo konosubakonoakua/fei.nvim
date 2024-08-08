f3be86f0db803df82db5f56c48c911fc8cfb70a3
konosubakonoakua <ailike_meow@qq.com>
2024-08-08 09:38:59 +0800

    feat: add `quickfix` to extras

--- lua/plugins/extras/editor/quickfix.lua
+++ lua/plugins/extras/editor/quickfix.lua
@@ -53,4 +53,44 @@ return {
       require("bqf").setup(opts)
     end,
   },
+
+  { -- https://github.com/stevearc/quicker.nvim
+    "stevearc/quicker.nvim",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {
+      keys = {
+        {
+          ">",
+          function()
+            require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+          end,
+          desc = "Expand quickfix context",
+        },
+        {
+          "<",
+          function()
+            require("quicker").collapse()
+          end,
+          desc = "Collapse quickfix context",
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>qk",
+        function()
+          require("quicker").toggle()
+        end,
+        desc = "Toggle quicker",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("quicker").toggle({ loclist = true })
+        end,
+        desc = "Toggle quicker loclist",
+      },
+    },
+  },
 }
