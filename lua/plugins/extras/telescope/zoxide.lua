1eebd3942bae4f3b5490bfbea495b459fd50d1ec
konosubakonoakua <ailike_meow@qq.com>
2024-09-23 13:50:15 +0800

    fix(zoxide): only override cmd on windows

--- lua/plugins/extras/telescope/zoxide.lua
+++ lua/plugins/extras/telescope/zoxide.lua
@@ -34,6 +34,8 @@ return {
 
     t.setup(opts)
     t.load_extension("zoxide")
-    vim.keymap.set("n", "<leader>sz", function() t.extensions.zoxide.list({ cmd = { "pwsh", "-nop", "-c", "zoxide query -ls" } }) end, { desc = "zoxide" })
+    vim.keymap.set("n", "<leader>sz", function() t.extensions.zoxide.list({
+      cmd = jit.os == "Windows" and { "pwsh", "-nop", "-c", "zoxide query -ls" } or nil,
+    }) end, { desc = "zoxide" })
   end,
 }
