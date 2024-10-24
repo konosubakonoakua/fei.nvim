1b00921b8e27b1de33bdde505181f36b03387e91
konosubakonoakua <ailike_meow@qq.com>
2024-10-24 10:39:15 +0800

    refactor(extras): use modicator

--- lua/plugins/extras/editor/vimmode.lua
+++ lua/plugins/extras/editor/vimmode.lua
@@ -22,26 +22,4 @@ return {
       },
     },
   },
-  {
-    "mvllow/modes.nvim",
-    enabled = false,
-    opts = {
-      line_opacity = 0.15,
-      set_cursor = true,
-      set_cursorline = true,
-      set_number = false,
-      ignore_filetypes = {
-        'NvimTree',
-        'TelescopePrompt',
-      }
-    },
-
-    config = function (_, opts)
-      vim.api.nvim_set_hl(0, "ModesCopy", { default = true, link = "lualine_a_normal" })
-      vim.api.nvim_set_hl(0, "ModesDelete", { default = true, link = "lualine_a_replace" })
-      vim.api.nvim_set_hl(0, "ModesInsert", { default = true, link = "lualine_a_insert" })
-      vim.api.nvim_set_hl(0, "ModesVisual", { default = true, link = "lualine_a_visual" })
-      require("modes").setup(opts)
-    end,
-  }
 }
