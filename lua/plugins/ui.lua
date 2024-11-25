07ae1cd8ddd7a2618f0a79337b29495a450706fc
konosubakonoakua <ailike_meow@qq.com>
2024-11-25 16:36:45 +0800

    feat: add random logos for snacks dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,7 @@
+local logos = require("util.stuffs.logos")
+local logo = logos[math.random(#logos)]
+print(logo)
+
 return {
 
   {
@@ -62,12 +66,22 @@ return {
 
   {
     "folke/snacks.nvim",
+    optional = true,
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
     opts = {
       bigfile = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = {
+        preset = { header = logo },
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+      },
       notifier = {
         enabled = true,
         timeout = 3000,
